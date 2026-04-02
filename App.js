217e4d96457ee6cfb1a5eac8063b0c6954dabfd0
arlumbiras <vitorrodrigueslima@hotmail.com>
2026-04-02 20:25:43 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import Exemplo01 from './src/exemplos/ex-01';
 import Exemplo02 from './src/exemplos/ex-02';
 import Exemplo03 from './src/exemplos/ex-03';
+import Exemplo04 from './src/exemplos/ex-04';
 
 import Atividade01 from './src/atividades/atv-01';
 import Atividade02 from './src/atividades/atv-02';
@@ -14,7 +15,7 @@ import Atividade03 from './src/atividades/atv-03';
 export default function App() {
   return (
     <View style={styles.container}>
-       <Atividade02/>
+       <Exemplo04/>
       <StatusBar style= "auto" />
     </View>
   );
