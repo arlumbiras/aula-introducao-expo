54809899e25a57d7d4e97ec6f7ba012332e1cf6a
VITOR RODRIGUES LIMA <vitor.lima202>
2026-04-14 22:49:52 -0300

    atv-06

--- App.js
+++ App.js
@@ -5,18 +5,22 @@ import Exemplo01 from './src/exemplos/ex-01';
 import Exemplo02 from './src/exemplos/ex-02';
 import Exemplo03 from './src/exemplos/ex-03';
 import Exemplo04 from './src/exemplos/ex-04';
+import Exemplo05 from './src/exemplos/ex-05';
+import Exemplo06 from './src/exemplos/ex-06';
 
 import Atividade01 from './src/atividades/atv-01';
 import Atividade02 from './src/atividades/atv-02';
 import Atividade03 from './src/atividades/atv-03';
 import Atividade04 from './src/atividades/atv-04';
+import Atividade05 from './src/atividades/atv-05';
+import Atividade06 from './src/atividades/atv-06';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-       <Atividade04/>
+       <Atividade06/>
       <StatusBar style= "auto" />
     </View>
   );
