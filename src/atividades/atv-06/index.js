9187c72c4e92d6b4d61d8d592ba9de258e1c03f6
VITOR RODRIGUES LIMA <vitor.lima202>
2026-04-28 19:14:47 -0300

    Update index.js

--- src/atividades/atv-06/index.js
+++ src/atividades/atv-06/index.js
@@ -13,12 +13,26 @@ export default function Atividade06() {
     const [altura, setAltura] = useState(null);
     const [imc, setImc] = useState(0.00);
     const mensErro = 'Valores inseridos de forma incorreta!';
+    const [mens, setMens] = useState('');
 
     function calculaImc() {
         const tmpImc = peso / (altura * altura);
         setImc(tmpImc);
     }
-
+    function setMens() {
+        if (calculaImc < 18.5) {
+        titulo = "Abaixo do peso";
+        texto = "Seu peso está abaixo do recomendado.";
+        classe = "azul-claro";
+       } else if (calculaImc < 25) {
+        titulo = "Peso normal";
+        texto = "Parabéns! Você está dentro do peso adequado.";
+        classe = "azul";
+         } else if (imc < 30) {
+        titulo = "Sobrepeso";
+        texto = "Atenção! Seu peso está acima do recomendado.";
+        classe = "laranja";
+      }
     return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Atividade 6</Text>
@@ -27,7 +41,7 @@ export default function Atividade06() {
                 <Input placeholder='Altura' valor={altura} atualizaValor={setAltura} />
             </View>
 
-            <Text style={styles.imc}>{isNaN(imc) ? mensErro :'O IMC é: \n' + imc.toFixed(2)}</Text>
+            <Text style={styles.imc}>{isNaN(imc) ? mensErro :'O IMC é: \n' + imc.toFixed(2) + }</Text>
 
             <Text style={[styles.imc, styles.txtMensagem]}>Mensagem</Text>
 
